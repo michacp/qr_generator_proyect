38931e8592630c75f5352d69527a217c551d749f
michacp <michacp@hotmail.com>
2026-05-13 22:50:06 -0500

    redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     react(),
   ],
+  // Redirects declarativos — Astro genera el HTML de redirect automáticamente
+  redirects: {
+    '/': '/es/',
+  },
 });
