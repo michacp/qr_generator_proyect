558a203b204981264caf31ff2706813ff9076f54
michacp <michacp@hotmail.com>
2026-05-16 11:24:46 -0500

    agregar ads

---
+++ src/components/ads/index.ts
@@ -0,0 +1,7 @@
+// src/components/ads/index.ts
+// Re-exports del módulo de publicidad.
+// Importa desde aquí para no depender de rutas internas:
+//   import { AdSidebar, AdBottom } from '../ads';
+
+export { default as AdSidebar } from './AdSidebar.astro';
+export { default as AdBottom }  from './AdBottom.astro';
